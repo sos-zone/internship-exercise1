47307ad35310d6024f4e8b37fcc3fd89f2689ce8
sergei <new_word@tut.by>
2016-02-16 23:54:11 +0300

    subtask 5 has finished

--- index.php
+++ index.php
@@ -4,27 +4,77 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exsercise 1</title>
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Exsercise1</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
   </head>
   <body>
+
+
+    <div class="jumbotron">
+
+      <div class="container">
+        <h2>My profile:</h2>
+        Name: <strong>Sergei</strong> <br>
+        Surname: <strong>Anzhenko</strong> <br>
+      </div>
+
+      <div class="container">
+        <h2>My hobby:</h2>
+        <ul>
+          <li>Programming</li>
+          <li>Skiing</li>
+          <li>Sauna</li>
+          <li>Swimming pool</li>
+          <li>Excursions</li>
+        </ul>
+      </div>
+
+    </div>      
+
+
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h2>Achievement 1</h2>
+            <p>I have been learning Google AdWord</p>
+            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <h2>Achievement 2</h2>
+            <p>I had got 1C-Bitrix Content-manager and Base-Administrator certificaties</p>
+            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+         </div>
+          <div class="col-md-4">
+            <h2>Achievement 3</h2>
+            <p>I had created building repair at bathroom</p>
+            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
     
-    <section>
-      <h2>My profile:</h2>
-      Name: <strong>Sergei</strong> <br>
-      Surname: <strong>Anzhenko</strong> <br>
-    </section>
 
-    <section>
-      <h2>My hobby:</h2>
-      <ul>
-        <li>Programming</li>
-        <li>Skiing</li>
-        <li>Sauna</li>
-        <li>Swimming pool</li>
-        <li>Excursions</li>
-      </ul>
-    </section>
-
-
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
