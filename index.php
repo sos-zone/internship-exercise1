88cafedc2f03ea5bfb0c9b63a9052226c02641f0
sergei <new_word@tut.by>
2016-02-16 22:58:54 +0300

    subtask 3 has finished

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exsercise 1</title>
+  </head>
+  <body>
+    
+    <section>
+      <h2>My profile:</h2>
+      Name: <strong>Sergei</strong> <br>
+      Surname: <strong>Anzhenko</strong> <br>
+    </section>
+
+    <section>
+      <h2>My hobby:</h2>
+      <ul>
+        <li>Programming</li>
+        <li>Skiing</li>
+        <li>Sauna</li>
+        <li>Swimming pool</li>
+        <li>Excursions</li>
+      </ul>
+    </section>
+
+
+  </body>
+</html>
